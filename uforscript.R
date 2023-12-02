e1c77bccf41f1c56175c103364569918b931473e
nhuynh011 <114946408+nhuynh011@users.noreply.github.com>
2023-12-02 10:52:33 -0800

    Add files via upload

---
+++ uforscript.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(janitor)
+library(here)
+
+#Clean dataset 
+ufo <- read_csv(here("rawdata", "ufo-sightings-transformed.csv")) |> clean_names()
+ufo<-count(ufo, country, sort = TRUE) |> na.omit(ufo)
+
+#Clean this dataset
+development <- read_csv(here("rawdata", "gdp-per-capita-worldbank.csv")) |> clean_names()
+development<- development |> filter(year == "2021") |> rename(country = entity)
+
+
+#Merge to make linear model
+ufogdp<- merge(x = ufo, y = development, by = "country", all.y = TRUE) |>
+  rename(sightings = n, gdpcapita = gdp_per_capita_ppp_constant_2017_international)
+ufogdp[is.na(ufogdp)] <- 0
+
+#Linear model on sightings by GDP
+sightingsModel<- lm(sightings ~ gdpcapita, data = ufogdp)
+summary(sightingsModel)
+ggplot(ufogdp, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')
+
+#This is messy, US is a huge outlier so I will remove that data point
+ufonoUS<- ufogdp |> filter(country != "United States")
+sightingsModelNoUS<- lm(sightings ~ gdpcapita, data = ufonoUS)
+summary(sightingsModelNoUS)
+ggplot(ufonoUS, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')
+
+#Linear model on sightings by area and GDP (not our topic but we can look at it)
