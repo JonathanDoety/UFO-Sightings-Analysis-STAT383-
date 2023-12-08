1fac275523e7e8be6d480144aa583ec347e1ecf7
armc <armc@armc.com>
2023-12-08 18:03:03 -0500

    Labelled axises for graphs and made graphs easier for viewing at the bottom of the end of the runnable code

--- uforscript.R
+++ uforscript.R
@@ -26,17 +26,24 @@ ufogdp[is.na(ufogdp)] <- 0
 #Linear model on sightings by GDP
 sightingsModel<- lm(sightings ~ gdpcapita, data = ufogdp)
 summary(sightingsModel)
-ufoplot<- ggplot(ufogdp, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')
+ufoplot<- ggplot(ufogdp, aes(gdpcapita, sightings)) +geom_point() +
+  geom_smooth(method = 'lm') + labs(title="UFO Sighitings by GDP Per Capita",
+                                  x ="GDP Per Capita ($)", y = "UFO Sightings")
 
 #This is messy, US is a huge outlier so I will remove that data point
 ufonoUS<- ufogdp |> filter(country != "United States")
 sightingsModelNoUS<- lm(sightings ~ gdpcapita, data = ufonoUS)
 summary(sightingsModelNoUS)
-noUSPlot<- ggplot(ufonoUS, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')
+noUSPlot<- ggplot(ufonoUS, aes(gdpcapita, sightings)) +geom_point() +
+geom_smooth(method = 'lm')+ labs(title="UFO Sighitings by GDP Per Capita (No U.S.)",
+                                  x ="GDP Per Capita ($)", y = "UFO Sightings")
+
+ufoplot
+noUSPlot
 
 #Look at both models at the same time:
 require(gridExtra)
-grid.arrange(noUSPlot, ufoplot)
+grid.arrange(ufoplot, noUSPlot)
 
 #Linear model on sightings by area and GDP (not our topic but we can look at it)
 
@@ -56,7 +63,9 @@ ufogdp_less500=ufogdp[ufogdp$sightings<500, ]
 #Linear model on sightings by GDP
 sightingsModel_less500<- lm(sightings ~ gdpcapita, data = ufogdp_less500)
 summary(sightingsModel_less500)
-ufo_less500plot<- ggplot(ufogdp_less500, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')
+ufo_less500plot<- ggplot(ufogdp_less500, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')+
+  labs(title="UFO Sighitings by GDP Per Capita (Sightings <500)",
+         x ="GDP Per Capita ($)", y = "UFO Sightings")
 grid.arrange(ufo_less500plot)
 
 
@@ -65,7 +74,9 @@ ufogdp_less150=ufogdp[ufogdp$sightings<150, ]
 #Linear model on sightings by GDP
 sightingsModel_less150<- lm(sightings ~ gdpcapita, data = ufogdp_less150)
 summary(sightingsModel_less150)
-ufo_less150plot<- ggplot(ufogdp_less150, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')
+ufo_less150plot<- ggplot(ufogdp_less150, aes(gdpcapita, sightings)) +geom_point() +geom_smooth(method = 'lm')+
+  labs(title="UFO Sighitings by GDP Per Capita (Sightings <150)",
+       x ="GDP Per Capita ($)", y = "UFO Sightings")
 grid.arrange(ufo_less150plot)
 
 #Look at both models at the same time:
@@ -76,16 +87,24 @@ grid.arrange(ufo_less500plot, ufo_less150plot)
 #use this
 #GGplot histogram sightings less than 500
 hist_less500 = ggplot(ufogdp_less500, aes(sightings))+geom_histogram(color="black",fill="white") +
-  geom_vline(aes(xintercept=mean(sightings)), color="blue", linetype="dashed", size=1)
+  geom_vline(aes(xintercept=mean(sightings)), color="blue", linetype="dashed", size=1)+
+  labs(title="Amount of UFO Sightings Histogram (Sightings <500)",
+       x ="UFO Sightings", y = "Number of Countries")
 
 #use this
 #GGplot histogram sightings less than 150
 hist_less150 = ggplot(ufogdp_less150, aes(sightings))+geom_histogram(color="black",fill="white") +
-  geom_vline(aes(xintercept=mean(sightings)), color="blue", linetype="dashed", size=1)
+  geom_vline(aes(xintercept=mean(sightings)), color="blue", linetype="dashed", size=1)+
+  labs(title="Amount of UFO Sightings Histogram (Sightings <150)",
+       x ="UFO Sightings", y = "Number of Countries")
 
-#Look at both models at the same time:
-require(gridExtra)
-grid.arrange(ufoplot, noUSPlot, ufo_less500plot, ufo_less150plot, hist_less500, hist_less150)
+
+ufoplot
+noUSPlot
+ufo_less500plot
+ufo_less150plot
+hist_less500
+hist_less150
 
 
 
